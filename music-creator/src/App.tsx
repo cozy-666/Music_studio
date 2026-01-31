0c45b6df0463a87b67ddba445d1453220fedfcb2
cozy-666 <cozy.dev.tech@gmail.com>
2026-01-31 14:37:27 +0900

    css書く場所確認

--- music-creator/src/App.tsx
+++ music-creator/src/App.tsx
@@ -2,13 +2,13 @@ import './App.css'
 
 function App() {
 
-  alert('javascriptを実行');
+  // alert('javascriptを実行');
 
   // リターンの中はHTMLを書きます。これより上にjavascriptを書きます
   return (
-    <>
+    <h1>
       こんにちは
-    </>
+    </h1>
   )
 }
 export default App
